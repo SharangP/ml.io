926e3733003a26232f7743ce3ed131b5f8848245
Sharang Phadke <sharang@neurotrack.com>
2014-09-01 17:36:08 -0700

    added basic cross validation

--- ml/Task.py
+++ ml/Task.py
@@ -1,5 +1,6 @@
 import numpy as np
-from sklearn import linear_model
+from sklearn import linear_model, cross_validation
+
 
 class Task(object):
     """
@@ -18,7 +19,7 @@ class Task(object):
 
 class MLTask(Task):
 
-    def __init__(self, schema, model, data, test = None):
+    def __init__(self, schema={}, model={}, data={}, test={}):
         self._schema, self._data, self._test = ({},) * 3
         self._model = ''
         self._results = {'score': None}
@@ -28,11 +29,15 @@ class MLTask(Task):
         self.set_test(test)
 
     def run(self):
-        self.train_model()
         if self._test:
+            self._model.fit(self._data['features'], self._data['targets'])
             self._results['score'] = self._model.score(self._test['features'], self._test['targets'])
         else:
-            self._results['score'] = 'yay'
+            X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+                    self._data['features'], self._data['targets'],
+                    test_size=self._schema['cross_validation_test_size'])
+            self._model.fit(X_train, y_train)
+            self._results['score'] = self._model.score(X_test, y_test)
 
     def results(self):
         return self._results['score']
@@ -41,7 +46,7 @@ class MLTask(Task):
         self._schema['desc'] = None if not s.has_key('desc') else s['desc']
         self._schema['feature_names'] = None if not s.has_key('feature_names') else s['feature_names']
         self._schema['target_names'] = None if not s.has_key('target_names') else s['target_names']
-        self._schema['cross_validation'] = False if not s.has_key('cross_validation') else s['cross_validation']
+        self._schema['cross_validation_test_size'] = 0.4 if not s.has_key('cross_validation_test_size') else s['cross_validation_test_size']
 
     def set_model(self, m):
         self._model = linear_model.LinearRegression()
@@ -51,11 +56,6 @@ class MLTask(Task):
         self._data['targets'] = np.array([]) if len(d['targets']) == 0 else d['targets']
 
     def set_test(self, d):
-        if d:
+        if len(d) > 0:
             self._test['features'] = np.array([]) if len(d['features']) == 0 else d['features']
             self._test['targets'] = np.array([]) if len(d['targets']) == 0 else d['targets']
-        else:
-            self._test = None
-
-    def train_model(self):
-        self._model.fit(self._data['features'], self._data['targets'])
