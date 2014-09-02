c4661087bccf636d109cac1d6148edba8731f45e
Sharang Phadke <sharang@neurotrack.com>
2014-09-01 20:30:30 -0700

    fixed Algorithms

--- ml/Algorithms.py
+++ ml/Algorithms.py
@@ -1,50 +1,50 @@
-import sklearn
+from sklearn import svm, neighbors, tree, ensemble, naive_bayes, lda, qda, cluster, linear_model
 
 CLASSIFIERS = {
         'Support Vector Machine' : {
-            'class' : sklearn.svm.SVC, 
+            'class' : svm.SVC, 
             'required_params' : ['kernel', 'C']
             },
         'K Neighbors Classifier' : {
-            'class' : sklearn.neighbors.KNeighborsClassifier,
+            'class' : neighbors.KNeighborsClassifier,
             'required_params' : ['k']
             },
         'Decision Tree Classifier' : {
-            'class' : sklearn.tree.DecisionTreeClassifier,
+            'class' : tree.DecisionTreeClassifier,
             'required_params' : ['max_depth']
             },
         'Random Forest Classifier' : {
-            'class' : sklearn.ensemble.RandomForestClassifier,
+            'class' : ensemble.RandomForestClassifier,
             'required_params' : ['max_depth', 'n_estimators', 'max_features']
             },
         'AdaBoost Classifier' : {
-            'class' : sklearn.ensemble.AdaBoostClassifier,
+            'class' : ensemble.AdaBoostClassifier,
             'required_params' : []
             },
         'Gaussian Naive Bayes' : {
-            'class' : sklearn.naive_bayes.GaussianNB,
+            'class' : naive_bayes.GaussianNB,
             'required_params' : []
             },
         'Linear Discriminant' : {
-            'class' : sklearn.lda.LDA,
+            'class' : lda.LDA,
             'required_params' : []
             },
         'Quadratic Discriminant' : {
-            'class' : sklearn.qda.QDA,
+            'class' : qda.QDA,
             'required_params' : []
             },
         }
 
 CLUSTERING = {
         'K Means' : {
-            'class' : sklearn.cluster.KMeans,
+            'class' : cluster.KMeans,
             'required_params' : ['k']
             }
         }
 
 REGRESSION = {
         'Linear Regression' : {
-            'class' : sklearn.linear_model.LinearRegression,
+            'class' : linear_model.LinearRegression,
             'required_params' : []
             }
         }
