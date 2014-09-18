a192ce0a980b0f0ea5a25effc7484d0449cbe61b
Sharang Phadke <sharang@neurotrack.com>
2014-09-17 22:43:30 -0700

    made Algorithms a single dictionary

--- ml/Algorithms.py
+++ ml/Algorithms.py
@@ -1,50 +1,50 @@
 from sklearn import svm, neighbors, tree, ensemble, naive_bayes, lda, qda, cluster, linear_model
 
-CLASSIFIERS = {
-        'Support Vector Machine' : {
-            'class' : svm.SVC, 
-            'required_params' : ['kernel', 'C']
-            },
-        'K Neighbors Classifier' : {
-            'class' : neighbors.KNeighborsClassifier,
-            'required_params' : ['k']
-            },
-        'Decision Tree Classifier' : {
-            'class' : tree.DecisionTreeClassifier,
-            'required_params' : ['max_depth']
-            },
-        'Random Forest Classifier' : {
-            'class' : ensemble.RandomForestClassifier,
-            'required_params' : ['max_depth', 'n_estimators', 'max_features']
-            },
-        'AdaBoost Classifier' : {
-            'class' : ensemble.AdaBoostClassifier,
-            'required_params' : []
-            },
-        'Gaussian Naive Bayes' : {
-            'class' : naive_bayes.GaussianNB,
-            'required_params' : []
-            },
-        'Linear Discriminant' : {
-            'class' : lda.LDA,
-            'required_params' : []
-            },
-        'Quadratic Discriminant' : {
-            'class' : qda.QDA,
-            'required_params' : []
-            },
+METHODS = {
+'Classifiers' : {
+    'Support Vector Machine' : {
+        'class' : svm.SVC, 
+        'required_params' : ['kernel', 'C']
+        },
+    'K Neighbors Classifier' : {
+        'class' : neighbors.KNeighborsClassifier,
+        'required_params' : ['k']
+        },
+    'Decision Tree Classifier' : {
+        'class' : tree.DecisionTreeClassifier,
+        'required_params' : ['max_depth']
+        },
+    'Random Forest Classifier' : {
+        'class' : ensemble.RandomForestClassifier,
+        'required_params' : ['max_depth', 'n_estimators', 'max_features']
+        },
+    'AdaBoost Classifier' : {
+        'class' : ensemble.AdaBoostClassifier,
+        'required_params' : []
+        },
+    'Gaussian Naive Bayes' : {
+        'class' : naive_bayes.GaussianNB,
+        'required_params' : []
+        },
+    'Linear Discriminant' : {
+        'class' : lda.LDA,
+        'required_params' : []
+        },
+    'Quadratic Discriminant' : {
+        'class' : qda.QDA,
+        'required_params' : []
+        },
+    },
+'Clustering' : {
+    'K Means' : {
+        'class' : cluster.KMeans,
+        'required_params' : ['k']
         }
-
-CLUSTERING = {
-        'K Means' : {
-            'class' : cluster.KMeans,
-            'required_params' : ['k']
-            }
+    },
+'Regression' : {
+    'Linear Regression' : {
+        'class' : linear_model.LinearRegression,
+        'required_params' : []
         }
-
-REGRESSION = {
-        'Linear Regression' : {
-            'class' : linear_model.LinearRegression,
-            'required_params' : []
-            }
-        }
+    }
+}
