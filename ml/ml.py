f6aaccfac7dd04aa41ff76bee036085ba0ac9bce
Sharang Phadke <sharang@neurotrack.com>
2014-09-19 18:41:30 -0700

    refactored mltask and renamed because git doesnt recognize lower case

--- ml/ml.py
+++ ml/ml.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn import linear_model, cross_validation
-from Task import Task
+from task import Task
 from algorithms import METHODS
 
 
@@ -14,19 +14,14 @@ class MLTask(Task):
 
         self._set_meta_data(meta_data)
         self._set_model(model)
-        self._set_data(data)
-        self._set_test(test)
+        self._set_training_data(data)
+        self._set_test_data(test)
 
     def run(self):
-        if self._test:
-            self._model.fit(self._data['features'], self._data['targets'])
-            self._results['score'] = self._model.score(self._test['features'], self._test['targets'])
-        else: #TODO: move cross validation into a method that can take various params
-            X_train, X_test, y_train, y_test = cross_validation.train_test_split(
-                    self._data['features'], self._data['targets'],
-                    test_size=self._meta_data['cross_validation_test_size'])
-            self._model.fit(X_train, y_train)
-            self._results['score'] = self._model.score(X_test, y_test)
+        if self._meta_data['cross_validation_run']:
+            self._cross_validate()
+        if self._meta_data['test_run']:
+            self._test()
 
     def results(self):
         return self._results['score']
@@ -35,21 +30,34 @@ class MLTask(Task):
         self._meta_data['desc'] = None if not s.has_key('desc') else s['desc']
         self._meta_data['feature_names'] = None if not s.has_key('feature_names') else s['feature_names']
         self._meta_data['target_names'] = None if not s.has_key('target_names') else s['target_names']
+        self._meta_data['cross_validation_run'] = False if not s.has_key('cross_validation_run') else s['cross_validation_run']
+        self._meta_data['test_run'] = False if not s.has_key('test_run') else s['test_run']
         self._meta_data['cross_validation_test_size'] = 0.4 if not s.has_key('cross_validation_test_size') else s['cross_validation_test_size']
 
     def _set_model(self, m):
         '''
         Set ML model based on input dict and Algorithms available
         '''
-        #TODO: ensure setting the model doesn't break better
+        #TODO: ensure setting the model doesn't break
         self._model = METHODS[m[0]][m[1]]['class']()
 
-    def _set_data(self, d):
+    def _set_training_data(self, d):
         if len(d):
             self._data['features'] = np.array([]) if len(d['features']) == 0 else d['features']
             self._data['targets'] = np.array([]) if len(d['targets']) == 0 else d['targets']
 
-    def _set_test(self, d):
+    def _set_test_data(self, d):
         if len(d):
             self._test['features'] = np.array([]) if len(d['features']) == 0 else d['features']
             self._test['targets'] = np.array([]) if len(d['targets']) == 0 else d['targets']
+            
+    def _cross_validate(self):
+        X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(
+                self._data['features'], self._data['targets'],
+                test_size=self._meta_data['cross_validation_test_size'])
+        self._model.fit(X_train, Y_train)
+        self._results['cross_validation_score'] = self._model.score(X_test, Y_test)
+            
+    def _test(self):
+        self._model.fit(self._data['features'], self._data['targets'])
+        self._results['test_score'] = self._model.score(self._test['features'], self._test['targets'])
--- ml/test.py
+++ ml/test.py
@@ -1,17 +1,20 @@
 from sklearn.datasets import load_iris
-from MLTask import MLTask
+from mltask import MLTask
 from algorithms import METHODS
 
 d = load_iris()
 data = {}
 data['features'] = d.data
 data['targets'] = d.target
-schema = {'cross_validation_test_size': 0.5}
+meta_data = {
+        'cross_validation_test_size': 0.5,
+        'cross_validation_run': True,
+        'test_run': False}
 for method in METHODS:
     for sub_method in METHODS[method]:
         try:
             print sub_method
-            ml = MLTask([method, sub_method], schema, data)
+            ml = MLTask([method, sub_method], meta_data, data)
             ml.run()
             r = ml.results()
             print r
